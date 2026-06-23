8a8381a8130e6e750210a9c9c27438c3ccff6052
ankitac474-svg <ankitac474@gmail.com>
2026-06-23 17:47:26 +0530

    install react router and configure navigation

---
+++ online-library/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Online Library</h1>
+
+      <h2>Categories</h2>
+
+      <ul>
+        <li>Fiction</li>
+        <li>Non-Fiction</li>
+        <li>Sci-Fi</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
