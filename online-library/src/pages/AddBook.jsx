4eb2d91d737cddadca358559b44cd6a251733908
ankitac474-svg <ankitac474@gmail.com>
2026-06-24 13:08:33 +0530

    create add book from page

--- online-library/src/pages/AddBook.jsx
+++ online-library/src/pages/AddBook.jsx
@@ -1,7 +1,37 @@
 function AddBook() {
   return (
     <div>
-      <h1>Add Book Page</h1>
+      <h1>Add New Book</h1>
+
+      <form>
+        <input
+          type="text"
+          placeholder="Book Title"
+        />
+        <br /><br />
+
+        <input
+          type="text"
+          placeholder="Author"
+        />
+        <br /><br />
+
+        <select>
+          <option>Fiction</option>
+          <option>Non-Fiction</option>
+          <option>Sci-Fi</option>
+        </select>
+        <br /><br />
+
+        <textarea
+          placeholder="Description"
+        ></textarea>
+        <br /><br />
+
+        <button type="submit">
+          Add Book
+        </button>
+      </form>
     </div>
   );
 }
