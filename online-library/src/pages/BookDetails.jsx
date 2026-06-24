cec6c67c8545d01062f87f73a634b9ab76f0b8bd
ankitac474-svg <ankitac474@gmail.com>
2026-06-24 13:04:59 +0530

    implement book details page with dynamic routing

--- online-library/src/pages/BookDetails.jsx
+++ online-library/src/pages/BookDetails.jsx
@@ -1,7 +1,24 @@
+import { useParams } from "react-router-dom";
+import { books } from "../data/books";
+
 function BookDetails() {
+  const { id } = useParams();
+
+  const book = books.find(
+    (book) => book.id === parseInt(id)
+  );
+
+  if (!book) {
+    return <h2>Book not found</h2>;
+  }
+
   return (
     <div>
-      <h1>Book Details Page</h1>
+      <h1>{book.title}</h1>
+      <h3>{book.author}</h3>
+      <p>Category: {book.category}</p>
+      <p>{book.description}</p>
+      <p>Rating: {book.rating}</p>
     </div>
   );
 }
