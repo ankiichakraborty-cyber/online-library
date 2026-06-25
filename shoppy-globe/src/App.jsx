91b6e718321fddaeab74c0f71995f017c86a76d1
ankitac474-svg <ankitac474@gmail.com>
2026-06-25 14:11:17 +0530

    Created responsive header component

--- shoppy-globe/src/App.jsx
+++ shoppy-globe/src/App.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div>
-      <h1>ShoppyGlobe</h1>
+    <>
+      <Header />
       <Outlet />
-    </div>
+    </>
   );
 }
 
--- shoppy-globe/src/components/Header/Header.jsx
+++ shoppy-globe/src/components/Header/Header.jsx
@@ -1,5 +1,19 @@
+import "../../styles/Header.css";
+import { Link } from "react-router-dom";
+
 function Header() {
-  return <h2>Header Component</h2>;
+  return (
+    <header className="header">
+      <div className="logo">
+        🛍️ <span>ShoppyGlobe</span>
+      </div>
+
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/checkout">Checkout</Link>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
