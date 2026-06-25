50cea521536d908eea5acfebaa805c0ed5c20a87
ankitac474-svg <ankitac474@gmail.com>
2026-06-25 16:46:52 +0530

    Implemented cart quantity controls and remove functionality

--- shoppy-globe/src/components/CartItem/CartItem.jsx
+++ shoppy-globe/src/components/CartItem/CartItem.jsx
@@ -1,19 +1,66 @@
 import PropTypes from "prop-types";
+import { useDispatch } from "react-redux";
+import {
+  increaseQuantity,
+  decreaseQuantity,
+  removeFromCart,
+} from "../../redux/cartSlice";
 
 function CartItem({ item }) {
+  const dispatch = useDispatch();
+
   return (
-    <div style={{ border: "1px solid #ccc", margin: "10px", padding: "10px" }}>
+    <div
+      style={{
+        border: "1px solid #ddd",
+        padding: "15px",
+        marginBottom: "15px",
+        borderRadius: "8px",
+        display: "flex",
+        alignItems: "center",
+        gap: "20px",
+      }}
+    >
       <img
         src={item.thumbnail}
         alt={item.title}
         width="100"
       />
 
-      <h3>{item.title}</h3>
+      <div style={{ flex: 1 }}>
+        <h3>{item.title}</h3>
 
-      <p>Price: ${item.price}</p>
+        <p>Price: ${item.price}</p>
 
-      <p>Quantity: {item.quantity}</p>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "10px",
+            marginTop: "10px",
+          }}
+        >
+          <button
+            onClick={() => dispatch(decreaseQuantity(item.id))}
+          >
+            -
+          </button>
+
+          <span>{item.quantity}</span>
+
+          <button
+            onClick={() => dispatch(increaseQuantity(item.id))}
+          >
+            +
+          </button>
+        </div>
+      </div>
+
+      <button
+        onClick={() => dispatch(removeFromCart(item.id))}
+      >
+        Remove
+      </button>
     </div>
   );
 }
--- shoppy-globe/src/redux/cartSlice.js
+++ shoppy-globe/src/redux/cartSlice.js
@@ -22,9 +22,45 @@ const cartSlice = createSlice({
         });
       }
     },
+
+    increaseQuantity: (state, action) => {
+      const item = state.items.find(
+        (item) => item.id === action.payload
+      );
+
+      if (item) {
+        item.quantity += 1;
+      }
+    },
+
+    decreaseQuantity: (state, action) => {
+      const item = state.items.find(
+        (item) => item.id === action.payload
+      );
+
+      if (item && item.quantity > 1) {
+        item.quantity -= 1;
+      }
+    },
+
+    removeFromCart: (state, action) => {
+      state.items = state.items.filter(
+        (item) => item.id !== action.payload
+      );
+    },
+
+    clearCart: (state) => {
+      state.items = [];
+    },
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const {
+  addToCart,
+  increaseQuantity,
+  decreaseQuantity,
+  removeFromCart,
+  clearCart,
+} = cartSlice.actions;
 
 export default cartSlice.reducer;
