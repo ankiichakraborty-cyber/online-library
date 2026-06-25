1601037806717b17e88ad44cdf64aa408b4dba95
ankitac474-svg <ankitac474@gmail.com>
2026-06-25 17:06:33 +0530

    Implemented product search using Redux

--- shoppy-globe/src/components/Header/Header.jsx
+++ shoppy-globe/src/components/Header/Header.jsx
@@ -1,13 +1,17 @@
 import "../../styles/Header.css";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { setSearchTerm } from "../../redux/searchSlice";
 
 function Header() {
   const cartItems = useSelector((state) => state.cart.items);
+  const dispatch = useDispatch();
 
   return (
     <header className="header">
-      <h1 className="logo">🛍️ ShoppyGlobe</h1>
+      <div className="logo">
+        🛍️ ShoppyGlobe
+      </div>
 
       <nav>
         <Link to="/">Home</Link>
@@ -16,8 +20,18 @@ function Header() {
           Cart ({cartItems.length})
         </Link>
 
-        <Link to="/checkout">Checkout</Link>
+        <Link to="/checkout">
+          Checkout
+        </Link>
       </nav>
+
+      <input
+        type="text"
+        placeholder="Search products..."
+        onChange={(e) =>
+          dispatch(setSearchTerm(e.target.value))
+        }
+      />
     </header>
   );
 }
--- shoppy-globe/src/components/ProductList/ProductList.jsx
+++ shoppy-globe/src/components/ProductList/ProductList.jsx
@@ -1,26 +1,41 @@
 import "../../styles/Product.css";
+import { useSelector } from "react-redux";
 import useProducts from "../../hooks/useProducts";
 import ProductItem from "../ProductItem/ProductItem";
 
 function ProductList() {
   const { products, loading, error } = useProducts();
 
+  const searchTerm = useSelector(
+    (state) => state.search.searchTerm
+  );
+
   if (loading) {
     return <h2>Loading products...</h2>;
   }
 
   if (error) {
-    return <h2>Error: {error}</h2>;
+    return <h2>{error}</h2>;
   }
+
+  const filteredProducts = products.filter((product) =>
+    product.title
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div className="products-container">
-      {products.map((product) => (
-        <ProductItem
-          key={product.id}
-          product={product}
-        />
-      ))}
+      {filteredProducts.length > 0 ? (
+        filteredProducts.map((product) => (
+          <ProductItem
+            key={product.id}
+            product={product}
+          />
+        ))
+      ) : (
+        <h2>No products found.</h2>
+      )}
     </div>
   );
 }
